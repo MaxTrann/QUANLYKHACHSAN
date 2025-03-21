18090bcf152673d8cea37c5f2c60bb7f5e4eafa2
MaxTrann <tranlequocdai2005@gmail.com>
2025-03-22 04:13:22 +0700

    Update TANG.cs

--- BusinessLayer/TANG.cs
+++ BusinessLayer/TANG.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,9 +16,61 @@ namespace BusinessLayer
         {
             db = Entities.CreateEntities();
         }
-        public List<tb_Tang> getAll()
+        public List<tb_Tang> getAll() // Lấy toàn bộ danh sách
         {
             return db.tb_Tang.ToList();
         }
+
+        public tb_Tang getItem(int id) // Lấy thông tin tầng theo ID
+        {
+            return db.tb_Tang.FirstOrDefault(x => x.IDTANG == id);
+        }
+
+        // thêm tầng mới
+        public void add(tb_Tang tang)
+        {
+            try
+            {
+                db.tb_Tang.Add(tang);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Có lỗi xảy ra trong quá trình xử lý dữ liệu." + ex.Message);
+            }
+        }
+
+        // Sửa thông tin tầng
+        public void update(tb_Tang tang)
+        {
+            var _tang = db.tb_Tang.FirstOrDefault(x => x.IDTANG ==  tang.IDTANG);
+            _tang.TENTANG = tang.TENTANG;
+            _tang.DISABLED = tang.DISABLED;
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Có lỗi xảy ra trong quá trình xử lý dữ liệu." + ex.Message);
+            }
+        }
+
+        // Xóa tầng
+        public void delete(int tang)
+        {
+            tb_Tang _tang = db.tb_Tang.FirstOrDefault(x => x.IDTANG == tang);
+            _tang.DISABLED = true;
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Có lỗi xảy ra trong quá trình xử lý dữ liệu." + ex.Message);
+            }
+        }
+
+
     }
 }
