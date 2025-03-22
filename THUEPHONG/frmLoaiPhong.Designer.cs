ac37dee0bf91d6cba00de2f3a8d041d9b0a61dd5
MaxTrann <tranlequocdai2005@gmail.com>
2025-03-22 12:04:49 +0700

    Thêm frm Loại Phòng

---
+++ THUEPHONG/frmLoaiPhong.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace THUEPHONG
+{
+    partial class frmLoaiPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLoaiPhong));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnThem = new System.Windows.Forms.ToolStripButton();
+            this.btnXoa = new System.Windows.Forms.ToolStripButton();
+            this.btnSua = new System.Windows.Forms.ToolStripButton();
+            this.btnLuu = new System.Windows.Forms.ToolStripButton();
+            this.btnBoQua = new System.Windows.Forms.ToolStripButton();
+            this.btnThoat = new System.Windows.Forms.ToolStripButton();
+            this.gcDanhSach = new DevExpress.XtraGrid.GridControl();
+            this.gvDanhSach = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.DISABLED = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.TENLOAIPHONG = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.DONGIA = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.SONGUOI = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.SOGIUONG = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chkDisabled = new System.Windows.Forms.CheckBox();
+            this.txtLoaiPhong = new System.Windows.Forms.TextBox();
+            this.nudDonGia = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudSoNguoi = new System.Windows.Forms.NumericUpDown();
+            this.nudSoGiuong = new System.Windows.Forms.NumericUpDown();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gcDanhSach)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvDanhSach)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDonGia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSoNguoi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSoGiuong)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnThem,
+            this.btnXoa,
+            this.btnSua,
+            this.btnLuu,
+            this.btnBoQua,
+            this.btnThoat});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(786, 47);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Image = ((System.Drawing.Image)(resources.GetObject("btnThem.Image")));
+            this.btnThem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(50, 44);
+            this.btnThem.Text = "Thêm";
+            this.btnThem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Image = ((System.Drawing.Image)(resources.GetObject("btnXoa.Image")));
+            this.btnXoa.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(39, 44);
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Image = ((System.Drawing.Image)(resources.GetObject("btnSua.Image")));
+            this.btnSua.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(38, 44);
+            this.btnSua.Text = "Sửa";
+            this.btnSua.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Image = ((System.Drawing.Image)(resources.GetObject("btnLuu.Image")));
+            this.btnLuu.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(37, 44);
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnBoQua
+            // 
+            this.btnBoQua.Image = ((System.Drawing.Image)(resources.GetObject("btnBoQua.Image")));
+            this.btnBoQua.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBoQua.Name = "btnBoQua";
+            this.btnBoQua.Size = new System.Drawing.Size(60, 44);
+            this.btnBoQua.Text = "Bỏ qua";
+            this.btnBoQua.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnBoQua.Click += new System.EventHandler(this.btnBoQua_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(51, 44);
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // gcDanhSach
+            // 
+            this.gcDanhSach.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gcDanhSach.Location = new System.Drawing.Point(0, 47);
+            this.gcDanhSach.MainView = this.gvDanhSach;
+            this.gcDanhSach.Name = "gcDanhSach";
+            this.gcDanhSach.Size = new System.Drawing.Size(786, 358);
+            this.gcDanhSach.TabIndex = 3;
+            this.gcDanhSach.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvDanhSach});
+            // 
+            // gvDanhSach
+            // 
+            this.gvDanhSach.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.DISABLED,
+            this.TENLOAIPHONG,
+            this.DONGIA,
+            this.SONGUOI,
+            this.SOGIUONG});
+            this.gvDanhSach.GridControl = this.gcDanhSach;
+            this.gvDanhSach.Name = "gvDanhSach";
+            this.gvDanhSach.CustomDrawCell += new DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventHandler(this.gvDanhSach_CustomDrawCell);
+            this.gvDanhSach.Click += new System.EventHandler(this.gvDanhSach_Click);
+            // 
+            // DISABLED
+            // 
+            this.DISABLED.Caption = "DEL";
+            this.DISABLED.FieldName = "DISABLED";
+            this.DISABLED.MaxWidth = 35;
+            this.DISABLED.MinWidth = 30;
+            this.DISABLED.Name = "DISABLED";
+            this.DISABLED.Visible = true;
+            this.DISABLED.VisibleIndex = 0;
+            this.DISABLED.Width = 35;
+            // 
+            // TENLOAIPHONG
+            // 
+            this.TENLOAIPHONG.Caption = "TÊN LOẠI PHÒNG";
+            this.TENLOAIPHONG.FieldName = "TENLOAIPHONG";
+            this.TENLOAIPHONG.MaxWidth = 250;
+            this.TENLOAIPHONG.MinWidth = 200;
+            this.TENLOAIPHONG.Name = "TENLOAIPHONG";
+            this.TENLOAIPHONG.Visible = true;
+            this.TENLOAIPHONG.VisibleIndex = 1;
+            this.TENLOAIPHONG.Width = 200;
+            // 
+            // DONGIA
+            // 
+            this.DONGIA.Caption = "ĐƠN GIÁ";
+            this.DONGIA.FieldName = "DONGIA";
+            this.DONGIA.MaxWidth = 140;
+            this.DONGIA.MinWidth = 120;
+            this.DONGIA.Name = "DONGIA";
+            this.DONGIA.Visible = true;
+            this.DONGIA.VisibleIndex = 2;
+            this.DONGIA.Width = 120;
+            // 
+            // SONGUOI
+            // 
+            this.SONGUOI.Caption = "SỐ NGƯỜI";
+            this.SONGUOI.FieldName = "SONGUOI";
+            this.SONGUOI.MaxWidth = 110;
+            this.SONGUOI.MinWidth = 100;
+            this.SONGUOI.Name = "SONGUOI";
+            this.SONGUOI.Visible = true;
+            this.SONGUOI.VisibleIndex = 3;
+            this.SONGUOI.Width = 100;
+            // 
+            // SOGIUONG
+            // 
+            this.SOGIUONG.Caption = "SỐ GIƯỜNG";
+            this.SOGIUONG.FieldName = "SOGIUONG";
+            this.SOGIUONG.MaxWidth = 120;
+            this.SOGIUONG.MinWidth = 100;
+            this.SOGIUONG.Name = "SOGIUONG";
+            this.SOGIUONG.Visible = true;
+            this.SOGIUONG.VisibleIndex = 4;
+            this.SOGIUONG.Width = 100;
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.nudSoGiuong);
+            this.groupControl1.Controls.Add(this.nudSoNguoi);
+            this.groupControl1.Controls.Add(this.label3);
+            this.groupControl1.Controls.Add(this.label2);
+            this.groupControl1.Controls.Add(this.label1);
+            this.groupControl1.Controls.Add(this.nudDonGia);
+            this.groupControl1.Controls.Add(this.txtLoaiPhong);
+            this.groupControl1.Controls.Add(this.label7);
+            this.groupControl1.Controls.Add(this.chkDisabled);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl1.Location = new System.Drawing.Point(0, 405);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(786, 135);
+            this.groupControl1.TabIndex = 4;
+            this.groupControl1.Text = "Thông tin";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(39, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Tên loại phòng";
+            // 
+            // chkDisabled
+            // 
+            this.chkDisabled.AutoSize = true;
+            this.chkDisabled.Location = new System.Drawing.Point(619, 48);
+            this.chkDisabled.Name = "chkDisabled";
+            this.chkDisabled.Size = new System.Drawing.Size(77, 20);
+            this.chkDisabled.TabIndex = 10;
+            this.chkDisabled.Text = "Disabled";
+            this.chkDisabled.UseVisualStyleBackColor = true;
+            // 
+            // txtLoaiPhong
+            // 
+            this.txtLoaiPhong.Location = new System.Drawing.Point(154, 47);
+            this.txtLoaiPhong.Name = "txtLoaiPhong";
+            this.txtLoaiPhong.Size = new System.Drawing.Size(187, 23);
+            this.txtLoaiPhong.TabIndex = 14;
+            // 
+            // nudDonGia
+            // 
+            this.nudDonGia.Location = new System.Drawing.Point(470, 47);
+            this.nudDonGia.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.nudDonGia.Name = "nudDonGia";
+            this.nudDonGia.Size = new System.Drawing.Size(120, 23);
+            this.nudDonGia.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(396, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Đơn giá";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Số người";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(382, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Số giường";
+            // 
+            // nudSoNguoi
+            // 
+            this.nudSoNguoi.Location = new System.Drawing.Point(154, 85);
+            this.nudSoNguoi.Name = "nudSoNguoi";
+            this.nudSoNguoi.Size = new System.Drawing.Size(120, 23);
+            this.nudSoNguoi.TabIndex = 19;
+            // 
+            // nudSoGiuong
+            // 
+            this.nudSoGiuong.Location = new System.Drawing.Point(470, 85);
+            this.nudSoGiuong.Name = "nudSoGiuong";
+            this.nudSoGiuong.Size = new System.Drawing.Size(120, 23);
+            this.nudSoGiuong.TabIndex = 20;
+            // 
+            // frmLoaiPhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 540);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.gcDanhSach);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "frmLoaiPhong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Danh mục Loại phòng";
+            this.Load += new System.EventHandler(this.frmLoaiPhong_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gcDanhSach)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvDanhSach)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDonGia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSoNguoi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSoGiuong)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnThem;
+        private System.Windows.Forms.ToolStripButton btnXoa;
+        private System.Windows.Forms.ToolStripButton btnSua;
+        private System.Windows.Forms.ToolStripButton btnLuu;
+        private System.Windows.Forms.ToolStripButton btnBoQua;
+        private System.Windows.Forms.ToolStripButton btnThoat;
+        private DevExpress.XtraGrid.GridControl gcDanhSach;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvDanhSach;
+        private DevExpress.XtraGrid.Columns.GridColumn DISABLED;
+        private DevExpress.XtraGrid.Columns.GridColumn TENLOAIPHONG;
+        private DevExpress.XtraGrid.Columns.GridColumn DONGIA;
+        private DevExpress.XtraGrid.Columns.GridColumn SONGUOI;
+        private DevExpress.XtraGrid.Columns.GridColumn SOGIUONG;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox chkDisabled;
+        private System.Windows.Forms.TextBox txtLoaiPhong;
+        private System.Windows.Forms.NumericUpDown nudSoGiuong;
+        private System.Windows.Forms.NumericUpDown nudSoNguoi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudDonGia;
+    }
+}
---
+++ THUEPHONG/frmLoaiPhong.cs
@@ -0,0 +1,160 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLayer;
+using DataLayer;
+
+namespace THUEPHONG
+{
+    public partial class frmLoaiPhong : DevExpress.XtraEditors.XtraForm
+    {
+        public frmLoaiPhong()
+        {
+            InitializeComponent();
+        }
+        LOAIPHONG _loaiphong;
+        bool _them;
+        int _idloaiphong;
+
+        private void frmLoaiPhong_Load(object sender, EventArgs e)
+        {
+            _loaiphong = new LOAIPHONG();
+            loadData();
+
+            showHideControl(true);
+            _enabled(false);
+        }
+
+        void loadData()
+        {
+            gcDanhSach.DataSource = _loaiphong.getAll();
+            gvDanhSach.OptionsBehavior.Editable = false;
+        }
+
+        void showHideControl(bool t)
+        {
+            btnThem.Visible = t;
+            btnSua.Visible = t;
+            btnXoa.Visible = t;
+            btnThoat.Visible = t;
+            btnLuu.Visible = !t;
+            btnBoQua.Visible = !t;
+        }
+
+        void _enabled(bool t)
+        {
+            txtLoaiPhong.Enabled = t;
+            nudDonGia.Enabled = t;
+            nudSoNguoi.Enabled = t;
+            nudSoGiuong.Enabled = t;
+            chkDisabled.Enabled = t;
+        }
+        void _reset()
+        {
+            txtLoaiPhong.Text = "";
+            nudDonGia.Value = 0;
+            nudSoNguoi.Value = 0;
+            nudSoGiuong.Value = 0;
+            chkDisabled.Text = "";
+            chkDisabled.Checked = false;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            _them = true;
+            showHideControl(false);
+            _enabled(true);
+            _reset();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn xóa không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                _loaiphong.delete(_idloaiphong);
+            }
+            loadData();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            _them = false;
+            _enabled(true);
+            showHideControl(false);
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (_them)
+            {
+                tb_LoaiPhong lp = new tb_LoaiPhong();
+                lp.TENLOAIPHONG = txtLoaiPhong.Text;
+                lp.DONGIA = (int)nudDonGia.Value;
+                lp.SONGUOI = (int) nudSoNguoi.Value;
+                lp.SOGIUONG = (int) nudSoGiuong.Value;
+                lp.DISABLED = chkDisabled.Checked;
+                _loaiphong.add(lp);
+            }
+            else
+            {
+                
+                tb_LoaiPhong lp = _loaiphong.getItem(_idloaiphong);
+                lp.TENLOAIPHONG = txtLoaiPhong.Text;
+                lp.DONGIA = (int)nudDonGia.Value;
+                lp.SONGUOI = (int)nudSoNguoi.Value;
+                lp.SOGIUONG = (int)nudSoGiuong.Value;
+                lp.DISABLED = chkDisabled.Checked;
+                lp.DISABLED = chkDisabled.Checked;
+                _loaiphong.update(lp);
+            }
+            _them = false;
+            loadData();
+            _enabled(false);
+            showHideControl(true);
+
+        }
+
+        private void btnBoQua_Click(object sender, EventArgs e)
+        {
+            _them = false;
+            showHideControl(true);
+            _enabled(false);
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void gvDanhSach_Click(object sender, EventArgs e)
+        {
+            if (gvDanhSach.RowCount > 0)
+            {
+                _idloaiphong = Convert.ToInt32(gvDanhSach.GetFocusedRowCellValue("IDLOAIPHONG"));
+                nudDonGia.Value = Convert.ToDecimal(gvDanhSach.GetFocusedRowCellValue("DONGIA"));
+                nudSoGiuong.Value = Convert.ToDecimal(gvDanhSach.GetFocusedRowCellValue("SOGIUONG"));
+                nudSoNguoi.Value = Convert.ToDecimal(gvDanhSach.GetFocusedRowCellValue("SONGUOI"));
+                chkDisabled.Checked = bool.Parse(gvDanhSach.GetFocusedRowCellValue("DISABLED").ToString());
+            }
+        }
+
+        private void gvDanhSach_CustomDrawCell(object sender, DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventArgs e)
+        {
+            if (e.Column.Name == "DISABLED" && bool.Parse(e.CellValue.ToString()) == true)
+            {
+                Image img = Properties.Resources.del_icon_28px;
+                e.Graphics.DrawImage(img, e.Bounds.X, e.Bounds.Y);
+                e.Handled = true;
+
+            }
+        }
+    }
+}
